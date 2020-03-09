e7648e5dd08f22ed9fe1801710983407d54b8f28
Code Nation <codenation@Codes-MacBook-Pro.local>
2020-03-09 11:56:05 +0000

    addition of close span for task removal in js

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 const newItem = () => {
-    let listItem = document.createElement("li");  // creating html li
+    let li = document.createElement("li");  // creating html li
     let newTask = document.getElementById("taskInput").value; // obtain value of input box
     let textNode = document.createTextNode(newTask); // create input text
-    listItem.appendChild(textNode); // add text as list item
+    li.appendChild(textNode); // add text as list item
 
     if(newTask === ""){
         alert("Please add a task")
     } else {
         document.getElementbyID("taskList").appendChild(listItem);  // show input text in browser as an li
     }
+
+    document.getElementById("taskInput").value = "";
+
+    let closeSpan = document.createElement("span");
+    let text = document.createTextNode("\u00D7");
+    closeSpan.className = "close";
+    closeSpan.appendChild(text);
+    li.appendChild(closeSpan);
 }
