25dcf5e665b7db23d5a0336975433b52b199f8a6
Code Nation <codenation@Codes-MacBook-Pro.local>
2020-03-17 13:38:35 +0000

    failsafe commit

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
+// let closeBtn = document.getElementsByClassName("close");
+
 const newItem = () => {
     let li = document.createElement("li");  // creating html li
     let newTask = document.getElementById("taskInput").value; // obtain value of input box
+    console.log(newTask);
     let textNode = document.createTextNode(newTask); // create input text
     li.appendChild(textNode); // add text as list item
 
-    if(newTask === ""){
+    if(newTask == ""){
         alert("Please add a task")
     } else {
         document.getElementbyID("taskList").appendChild(listItem);  // show input text in browser as an li
@@ -17,4 +20,12 @@ const newItem = () => {
     closeSpan.className = "close";
     closeSpan.appendChild(text);
     li.appendChild(closeSpan);
+
+    // remove item when clicked
+    for (i = 0; i < closeBtn.length; i++) {
+        closeBtn[i].onclick = function () {
+            let parentDiv = this.parentElement;
+            parentDiv.style.display = "none";
+        }
+    }
 }
